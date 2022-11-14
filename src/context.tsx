e7d6e18e90733b071f08295fa6cf367f33457365
Steve Kinney <hello@stevekinney.net>
2022-11-14 14:24:17 -0700

    Implement the Context API with TypeScript
    Add items to the context

--- src/context.tsx
+++ src/context.tsx
@@ -1,19 +1,60 @@
 import { useState, createContext, PropsWithChildren } from 'react';
-// import {
-//   createItem,
-//   filterItems,
-//   getInitialItems,
-//   removeItem,
-//   updateItem,
-// } from './lib/items';
+import {
+  createItem,
+  filterItems,
+  getInitialItems,
+  removeItem,
+  updateItem,
+} from './lib/items';
 
-const ItemsContext = createContext(null);
+type ItemsState = {
+  items: Item[];
+  packedItems: Item[];
+  unpackedItems: Item[];
+  add: (name: string) => void;
+  remove: (id: string) => void;
+  update: (id: string, updates: any) => void;
+  markAllAsUnpacked: () => void;
+};
+
+export const ItemsContext = createContext({} as ItemsState);
 
 const ItemsProvider = ({ children }: PropsWithChildren) => {
-  // eslint-disable-next-line
-  const [items, setItems] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>(getInitialItems());
 
-  return <ItemsContext.Provider value={null}>{children}</ItemsContext.Provider>;
+  const add = (name: string) => {
+    const item = createItem(name);
+    setItems([...items, item]);
+  };
+
+  const update = (id: string, updates: any) => {
+    setItems(updateItem(items, id, updates));
+  };
+
+  const remove = (id: string) => {
+    setItems(removeItem(items, id));
+  };
+
+  const unpackedItems = filterItems(items, { packed: false });
+  const packedItems = filterItems(items, { packed: true });
+
+  const markAllAsUnpacked = () => {
+    return setItems(items.map((item) => ({ ...item, packed: false })));
+  };
+
+  const value = {
+    items,
+    packedItems,
+    unpackedItems,
+    add,
+    remove,
+    update,
+    markAllAsUnpacked,
+  };
+
+  return (
+    <ItemsContext.Provider value={value}>{children}</ItemsContext.Provider>
+  );
 };
 
 export default ItemsProvider;
